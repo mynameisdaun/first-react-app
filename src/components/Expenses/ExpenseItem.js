2447e8d498bd408ead52e1e764017ab24eed12af
mynameisdaun <daun9870.jung@gmail.com>
2022-07-07 22:32:52 +0900

    module1 complete

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,17 @@
 //component in react is just a function... that returns html code!
 import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) { // react use just only '1' argument
 	return (
-		<div className="expense-item">
-			<div>
-				<div>{props.date.toLocaleString('en-US', {month:'long'})}</div>
-			</div>
+		<Card className="expense-item">
+			<ExpenseDate date={props.date}></ExpenseDate>
 			<div className="expense-item__description">
 				<h2>{props.title}</h2>
 				<div className="expense-item__price">{props.amount}</div>
 			</div>
-		</div>
+		</Card>
 	);
 }
 
